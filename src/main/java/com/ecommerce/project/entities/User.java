9d529d5ee8198891375da73d6614e74f3aa58b07
Bruno Souza <brunocloud78@gmail.com>
2024-11-15 18:20:00 -0300

    Criação user repository e Authentication Service para validar login

--- src/main/java/com/ecommerce/project/entities/User.java
+++ src/main/java/com/ecommerce/project/entities/User.java
@@ -1,6 +1,11 @@
 package com.ecommerce.project.entities;
 
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.ecommerce.project.enus.UserRole;
 
@@ -28,8 +33,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of= "id")
-public class User {
+public class User implements UserDetails{
 	
+	
+	private static final long serialVersionUID = 1L;
+
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -48,5 +56,18 @@ public class User {
     private List<Address> addresses;; 
 
     @Enumerated(EnumType.STRING) 
-    private UserRole role; 
+    private UserRole role;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+		else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+				
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return email;
+	} 
 }
